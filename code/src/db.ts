4957f7477e5325b14fe5ec14c1f17bd81ac9cfa5
Gabriele Petronella <gabriele@buildo.io>
2019-10-30 12:15:25 +0100

    Add weights when selecting user

--- code/src/db.ts
+++ code/src/db.ts
@@ -1,5 +1,7 @@
-import { Client } from "pg";
+import { Client, types } from "pg";
 import { EventBody } from "./model";
+
+types.setTypeParser(types.builtins.INT8, val => parseInt(val));
 
 export const sendToMetro = async (body: EventBody) => {
   const client = new Client();
@@ -10,4 +12,22 @@ export const sendToMetro = async (body: EventBody) => {
   };
   await client.query(query);
   await client.end();
+};
+
+export type ChoreScore = { user: string; count: number };
+
+export const getChoreScores = async (): Promise<ChoreScore[]> => {
+  const client = new Client();
+  await client.connect();
+  const query = {
+    text: `
+    SELECT "metro"."turno_chores_view"."user" AS "user", count(*) AS "count"
+    FROM "metro"."turno_chores_view"
+    GROUP BY "metro"."turno_chores_view"."user"
+    ORDER BY "count" DESC, "metro"."turno_chores_view"."user" ASC
+    `
+  };
+  const res = await client.query(query);
+  await client.end();
+  return res.rows;
 };
--- code/src/users.ts
+++ code/src/users.ts
@@ -1,6 +1,10 @@
-import sample = require("lodash.sample");
 import { WebClient } from "@slack/web-api";
 import { User } from "./model";
+import { getChoreScores, ChoreScore } from "./db";
+import * as Chance from "chance";
+import zip = require("lodash.zip");
+
+const chance = new Chance();
 
 // Top-level variables belong to the lambda execution context, which will stay around for a while
 // across lambda executions. We use it as a cache for Slack API requests.
@@ -28,5 +32,26 @@ async function slackUsers(web: WebClient): Promise<Array<User>> {
 
 export const randomUser = async (web: WebClient): Promise<User> => {
   const users = await slackUsers(web);
-  return sample(users)!;
+  const choreScores = await getChoreScores();
+  return sampleUser(users, choreScores);
 };
+
+/**
+ * Sample users weighted by the reverse of the number of chores they have done.
+ * If you have done more chores, you are selected with a lower probability.
+ */
+function sampleUser(users: User[], choreScores: ChoreScore[]): User {
+  const totalCount = choreScores.reduce((tot, score) => tot + score.count, 0);
+  const weightedScores = users.map(user => {
+    const score = choreScores.find(s => s.user === user.name);
+    if (!score) {
+      console.log(`Could not find score for ${user.name}`);
+    }
+    const scoreCount = (score && score.count) || 0;
+    return totalCount - scoreCount;
+  });
+
+  console.log(zip(users.map(u => u.name), weightedScores).join("\n"));
+
+  return chance.weighted(users, weightedScores);
+}
